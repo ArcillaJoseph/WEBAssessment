3d076cda59c7b8c92254116fc723bf25aa767843
Ace Raphael Erive <joseph.arcilla@goldenlink.ph>
2024-09-30 11:25:00 +0800

    Add conditionals and loops in JavaScript
    Add conditionals and loops in JavaScript

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
 function showAlert() {
     alert("Button was clicked!");
+
+    // Prompt for a number
+    const userInput = prompt("Please enter a number:");
+    const number = parseInt(userInput, 10);
+
+    // Check if the number is even or odd
+    if (!isNaN(number)) {
+        if (number % 2 === 0) {
+            alert(number + " is even.");
+        } else {
+            alert(number + " is odd.");
+        }
+
+        // Log numbers from 1 to the user's number
+        for (let i = 1; i <= number; i++) {
+            console.log(i);
+        }
+    } else {
+        alert("That's not a valid number!");
+    }
 }
